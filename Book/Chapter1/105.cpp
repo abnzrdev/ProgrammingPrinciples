3d663a74a91f3606139d758473843a51827fec91
Abenezer <abnzr.dev@gmail.com>
2025-02-13 04:19:53 +0500

    Showing program the case sensitive

---
+++ Book/Chapter1/105.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Abenezer on 2/12/2025.
+//showing that a program is case sensitive updated through trial and error reading compiler messages
+#include <iostream>
+
+int main(){
+  char s[64] = "Goodbye, cruel world! ";
+  std::cout << s << "\n";
+  return 0;
+}
+
+

7aff644baa967aa1ec6280bcf8641f5a5af1a2f3
Abenezer <abnzr.dev@gmail.com>
2025-02-26 02:16:51 +0500

    Currency converter using selection statement(if statement used)

---
+++ Book/Chapter3/102.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Abenezer on 2/26/2025.
+// Currency converter
+#include <iostream>
+using namespace std;
+
+int main(){
+  constexpr double yen_to_usd = 0.0067;
+  constexpr double krn_to_usd = 0.09;
+  constexpr double yen_to_krn = 0.075;
+  char currency = ' ';
+
+  double cash;
+  cout << "Enter the amount of cash you have with the currency you hold(u = usd , y = yen , k = krone) : ";
+  cin >>  cash >> currency;
+
+  if(currency == 'u'){
+    cout <<  "You have " << cash / yen_to_usd << "yen" << endl
+         << "Your balance is " << cash / krn_to_usd << "krn!";
+  }
+  else if(currency == 'k'){
+    cout <<  "You have " << cash / yen_to_krn << "yen" << endl
+         << "Your balance is " << cash * krn_to_usd << " usd!";
+  }
+  else if (currency == 'y'){
+      cout <<  "You have " << cash * yen_to_usd << "usd" << endl
+           << "Your balance is " << cash * yen_to_krn << "krn!";
+  }
+  else{
+    cout << " I don't know the currency";
+  }
+  return 0;
+}

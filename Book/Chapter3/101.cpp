05646f0af2842ace4452cb1ce56262e500d2f145
Abenezer <abnzr.dev@gmail.com>
2025-02-26 02:14:24 +0500

    CM to inch converter made by if statements

---
+++ Book/Chapter3/101.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Abenezer on 2/26/2025.
+// Cm to In converter
+#include <iostream>
+using namespace std;
+
+int main(){
+  constexpr double cm_per_inch = 2.54;
+  int length = 0;
+  char unit = ' ';
+
+  cout << "Enter the length with the corresponding unit(i/c) : ";
+  cin >> length >> unit;
+
+  if(unit == 'i'){
+    cout << "The lenght is " << length * cm_per_inch << " cm!";
+  }
+  else if(unit == 'c') {
+    cout << "The length is "  << length / cm_per_inch << " inch!";
+  }
+  else {
+    cout << "I don't know the unit!!!";
+  }
+  return 0;
+}

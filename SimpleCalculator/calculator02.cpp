966704316e54de8ef5c1202c600500771302e33f
Abenezer <abnzr.dev@gmail.com>
2025-06-02 16:53:58 +0500

    Simple calculator program is started

---
+++ SimpleCalculator/calculator02.cpp
@@ -0,0 +1,169 @@
+//
+// Created by Abenezer on 5/28/2025.
+//
+#include "../book/chapter5/std_lib_facilities.h"  // Calling header files for the program
+
+//------------------------------------------------------------------------------
+
+class Token {
+public:
+    char kind;
+    double value;
+
+    Token(char ch) : kind(ch), value(0) { }
+    Token(char ch, double val) : kind(ch), value(val) { }
+};
+
+//------------------------------------------------------------------------------
+
+class Token_stream {
+public:
+    Token_stream();
+    Token get();
+    void putback(Token t);
+
+private:
+    bool full;
+    Token buffer;
+};
+
+//------------------------------------------------------------------------------
+
+Token_stream::Token_stream()
+    : full(false), buffer(0) { }  // buffer initialized with a dummy token
+
+void Token_stream::putback(Token t) {
+    if (full) error("putback() into a full buffer");
+    buffer = t;
+    full = true;
+}
+
+Token Token_stream::get() {
+    if (full) {
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;
+
+    switch (ch) {
+    case '=': case 'x': case '(': case ')': case '{':
+    case '+': case '-': case '*': case '/': case  '}':
+        return Token(ch);
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9': {
+        cin.putback(ch);
+        double val;
+        cin >> val;
+        return Token('8', val);  // '8' represents a number
+    }
+    default:
+        error("Bad token");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+Token_stream ts;  // global Token_stream
+
+// Forward declarations
+double expression();
+double term();
+double primary();
+
+//------------------------------------------------------------------------------
+
+double primary() {
+    Token t = ts.get();
+    switch (t.kind) {
+
+    case '(': {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != ')') error("')' expected");
+        return d;
+    }
+    case '8':
+        return t.value;
+    default:
+        error("primary expected");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+double term() {
+    double left = primary();
+    Token t = ts.get();
+
+    while (true) {
+        switch (t.kind) {
+        case '*':
+            left *= primary();
+            t = ts.get();
+            break;
+        case '/': {
+            double d = primary();
+            if (d == 0) error("divide by zero");
+            left /= d;
+            t = ts.get();
+            break;
+        }
+        default:
+            ts.putback(t);
+            return left;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+double expression() {
+    double left = term();
+    Token t = ts.get();
+
+    while (true) {
+        switch (t.kind) {
+        case '+':
+            left += term();
+            t = ts.get();
+            break;
+        case '-':
+            left -= term();
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t);
+            return left;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+int main() {
+    try {
+        cout << "Welcome to our simple calculator.\n";
+        cout << "Please enter expressions using floating-point numbers.\n";
+        cout << "Operators: + - * / and parentheses (). Type '=' to print and 'x' to quit.\n";
+
+        while (cin) {
+            cout << "> ";
+            Token t = ts.get();
+            while (t.kind == '=') t = ts.get();  // Skip repeated print commands
+            if (t.kind == 'x') break;
+            ts.putback(t);
+            double val = expression();
+            cout << "The Value is : " << val << '\n';
+        }
+        return 0;
+    } catch (exception& e) {
+        cerr << "error: " << e.what() << '\n';
+        return 1;
+    } catch (...) {
+        cerr << "Oops: unknown exception!\n";
+        return 2;
+    }
+}
